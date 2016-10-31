d323e14be457df0ebca906a601d4bcdce93eb35d
Prabodh Meshram <prabodh.meshram7@gmail.com>
2016-10-31 20:14:05 +0530

    Added Css and Updated App
    - Adding Css to remove li default style
    - Adding Extra properties to show pizza.

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ var app = angular.module("pizzaApp", []);
 
 app.controller('PizzaController',  function($scope){
 	$scope.message = "This is my first Pizaa App";
+
+	$scope.PizzaData = [{name:"Margaretta", prize: 34},{name:"Veg", prize: 30},{name:"Supreme", prize: 45},{name:"Italian Delight", prize: 40}];
 });
