777334bff2cfacadab9153a7b6728d44d93955b4
Prabodh Meshram <prabodh.meshram7@gmail.com>
2016-11-04 00:08:52 +0530

    Duplicating the Data

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ var app = angular.module("pizzaApp", []);
 app.controller('PizzaController',  function($scope){
 	$scope.message = "This is my first Pizaa App";
 
-	$scope.PizzaData = [{name:"Margaretta", prize: 34},{name:"Veg", prize: 30},{name:"Supreme", prize: 45},{name:"Italian Delight", prize: 40}];
+	$scope.PizzaData = [
+							{name:"Margaretta", prize: 34},
+							{name:"Veg", prize: 30},
+							{name:"Supreme", prize: 45},
+							{name:"Italian Delight", prize: 40},
+							{name:"Margaretta", prize: 34},
+							{name:"Veg", prize: 30},
+							{name:"Supreme", prize: 45},
+							{name:"Italian Delight", prize: 40},
+							{name:"Margaretta", prize: 34},
+							{name:"Veg", prize: 30},
+							{name:"Supreme", prize: 45},
+							{name:"Italian Delight", prize: 40}
+						];
 });
 
 app.controller('TitleController', ['$scope', function($scope){
